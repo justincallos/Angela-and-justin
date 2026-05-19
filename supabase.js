d7777e6794feb4ed5f54a3bb9fd5a1cd2fd70b1e
justincallos <justincallos@gmail.com>
2026-05-20 00:04:05 +1000

    Delete supabase.js
    U

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://sskndsmrtmdrzdllncmx.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_lc-oDVuoTaYPU0f-NJtWLg_puIYgu_N';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
